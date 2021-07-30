d7992bbb14d21130322dc76d9ea5930d74c58b41
penehyba <penehyba@redhat.com>
2021-07-30 10:45:58 +0200

    load PUBLIC_KEY from pem file

--- src/main/java/io/openshift/example/RestApplication.java
+++ src/main/java/io/openshift/example/RestApplication.java
@@ -16,25 +16,37 @@
  */
 package io.openshift.example;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Paths;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.json.JsonObject;
-import io.vertx.core.logging.Logger;
-import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.auth.PubSecKeyOptions;
+import io.vertx.ext.auth.authorization.AuthorizationProvider;
+import io.vertx.ext.auth.authorization.PermissionBasedAuthorization;
 import io.vertx.ext.auth.jwt.JWTAuth;
 import io.vertx.ext.auth.jwt.JWTAuthOptions;
+import io.vertx.ext.auth.jwt.authorization.MicroProfileAuthorization;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.JWTAuthHandler;
 import io.vertx.ext.web.handler.StaticHandler;
 import io.openshift.example.service.Greeting;
+import org.apache.commons.io.IOUtils;
 
 import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
 public class RestApplication extends AbstractVerticle {
-
-  private final Logger log = LoggerFactory.getLogger(RestApplication.class);
+  private static String PUBLIC_KEY;
   private long counter;
+
+  static {
+    try {
+      PUBLIC_KEY = IOUtils.toString(Paths.get("src", "test", "resources", "public.pem").toFile().toURI(), Charset.defaultCharset());
+    } catch (IOException e) {
+      System.err.println("Unable to load PUBLIC_KEY from pem file!");
+    }
+  }
 
   @Override
   public void start(Promise<Void> done) {
@@ -50,25 +62,29 @@ public class RestApplication extends AbstractVerticle {
       .put("credentials", new JsonObject()
         .put("secret", System.getenv("SECRET")));
 
+    JsonObject config = new JsonObject()
+      // since we're consuming keycloak JWTs we need
+      // to locate the permission claims in the token
+      .put("permissionsClaimKey", "realm_access/roles");
     // Configure the AuthHandler to process JWT's
     router.route("/api/greeting").handler(JWTAuthHandler.create(
-      JWTAuth.create(vertx, new JWTAuthOptions()
+      JWTAuth.create(vertx, new JWTAuthOptions(config)
         .addPubSecKey(new PubSecKeyOptions()
           .setAlgorithm("RS256")
-          .setPublicKey(System.getenv("REALM_PUBLIC_KEY")))
-        // since we're consuming keycloak JWTs we need to locate the permission claims in the token
-        .setPermissionsClaimKey("realm_access/roles"))));
+          .setBuffer(PUBLIC_KEY)))));
 
     // This is how one can do RBAC, e.g.: only admin is allowed
-    router.get("/api/greeting").handler(ctx ->
-      ctx.user().isAuthorized("booster-admin", authz -> {
-        if (authz.succeeded() && authz.result()) {
+    router.get("/api/greeting").handler(ctx -> {
+      AuthorizationProvider authorizationProvider = MicroProfileAuthorization.create();
+      authorizationProvider.getAuthorizations(ctx.user(), voidAsyncResult -> {
+        if(voidAsyncResult.succeeded() & PermissionBasedAuthorization.create("booster-admin").match(ctx.user())) {
           ctx.next();
         } else {
-          log.error("AuthZ failed!");
+          System.err.println("AuthZ failed!");
           ctx.fail(403);
         }
-      }));
+      });
+    });
 
     router.get("/api/greeting").handler(ctx -> {
       String name = ctx.request().getParam("name");
